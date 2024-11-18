9bae4f520b9460a28514f0ca8a6712fd578ff35c
michael <michaelreques@gmail.com>
2024-11-18 07:23:27 -0300

    feat: update fonts to mulish

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
-import { dark } from "@clerk/themes";
 import { redirect } from "next/navigation";
 // import { Button } from "./_components/ui/button";
 
@@ -13,7 +12,7 @@ const Home = async () => {
 
   return (
     <div className="flex h-full items-center justify-center">
-      <UserButton showName appearance={{ baseTheme: dark }} />
+      <UserButton showName />
     </div>
   );
 };
